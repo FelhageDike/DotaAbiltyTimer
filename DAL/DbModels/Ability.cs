e83f90a104f971d0ee7db67ecd2db4dfc37aea3e
FelhageDike <tavroslive@mail.ru>
2023-06-28 00:24:09 +0400

    init

--- DAL/DbModels/Ability.cs
+++
@@ -1,8 +0,0 @@
-namespace DAL.DbModels;
-
-public class Ability
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int Cooldown { get; set; }
-}
--- DAL/DbModels/Hero.cs
+++
@@ -1,8 +0,0 @@
-namespace DAL.DbModels;
-
-public class Hero
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public Ability Abilities { get; set; }
-}
